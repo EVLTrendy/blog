bae1791acb619d8f74f9f537eac86a310d50ac73
EVLTrendy <141459487+EVLTrendy@users.noreply.github.com>
2023-11-19 20:57:07 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,9 @@
-const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/fonts.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
-
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        if (!(dateObj instanceof Date)) {
-            console.error("Invalid date object:", dateObj);
-            return "Invalid Date";
-        }
-
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    });
 
     return {
         dir: {
